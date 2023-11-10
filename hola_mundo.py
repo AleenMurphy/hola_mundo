d596f76a200597d87677a34f4659b9ff22e9490c
AleenMurphy <aleenmurphy@gmail.com>
2023-11-10 01:46:38 -0300

    commit

---
+++ hola_mundo.py
@@ -0,0 +1,13 @@
+print("Hola, mundo")
+name = "Aleen"
+print("Hola, Aleen")
+print("Hola " + "Aleen")
+name = 18
+print("Hola", 18)
+print("Hola " + str(18))
+fave_food1 = "sushi"
+fave_food2 = "lasagna"
+print("Amo comer {} y {}".format(fave_food1, fave_food2))
+print(f"Amo comer {fave_food1} y {fave_food2}")
+
+
